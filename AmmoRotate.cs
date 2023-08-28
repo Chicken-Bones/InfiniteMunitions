55dccef30c4d28f10a72c4e337799ab93ae69ead
Chicken-Bones <mehvids@gmail.com>
2023-08-28 15:37:37 +1000

    Use new Player.ChooseAmmo method for current ammo display

--- AmmoRotate.cs
+++ AmmoRotate.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using Terraria;
 using Terraria.Audio;
@@ -15,14 +15,7 @@ namespace InfiniteMunitions
 
 		public override bool CanRotate(Player player, Item weapon) => GetAmmoType(weapon) > 0;
 
-		public override Item GetUI(Player player, Item weapon) {
-			int ammoType = GetAmmoType(weapon);
-			if (ammoType <= 0)
-				return null;
-
-			var slots = GetAmmoSlots(player, ammoType);
-			return slots.Count > 0 ? player.inventory[slots[0]] : null;
-		}
+		public override Item GetUI(Player player, Item weapon) => player.ChooseAmmo(weapon);
 
 		public override void Rotate(Player player, Item weapon, int offset, bool scroll) {
 			int ammoType = GetAmmoType(weapon);
